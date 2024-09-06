303473b76e9cab9047b13450fa36be6327808e29
ishimwe-moses <160502579+ishimwe-moses@users.noreply.github.com>
2024-09-06 11:18:42 +0200

    Update oceania exam.js

--- oceania exam.js
+++ oceania exam.js
@@ -455,7 +455,7 @@ let selectedQuestions = [];
 let correctAnswers = 0;
 
 function startTest() {
-    selectedQuestions = questions.sort(() => 0.5 - Math.random()).slice(0, 50);
+    selectedQuestions = questions.sort(() => 0.5 - Math.random()).slice(0, 500000);
     displayQuestions();
     document.getElementById('instructions').style.display = 'none';
     document.getElementById('quiz').style.display = 'block';

3d881246a06a1769c6c369bad30b16519a4b3731
ishimwe-moses <160502579+ishimwe-moses@users.noreply.github.com>
2024-09-06 11:12:24 +0200

    Update north america quiz1.js

--- north america quiz1.js
+++ north america quiz1.js
@@ -447,7 +447,7 @@ let selectedQuestions = [];
 let correctAnswers = 0;
 
 function startTest() {
-    selectedQuestions = questions.sort(() => 0.5 - Math.random()).slice(0, 20);
+    selectedQuestions = questions.sort(() => 0.5 - Math.random()).slice(0, 2000000);
     displayQuestions();
     document.getElementById('instructions').style.display = 'none';
     document.getElementById('quiz').style.display = 'block';

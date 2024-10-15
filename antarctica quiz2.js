bda0128c271782f9c83ecae421f59afe9c0936f1
ishimwe-moses <160502579+ishimwe-moses@users.noreply.github.com>
2024-10-15 16:14:31 +0200

    Update antarctica quiz2.js

--- antarctica quiz2.js
+++ antarctica quiz2.js
@@ -295,6 +295,8 @@ function displayResults(corrections) {
             <p class="correct">Correct Answer: ${Array.isArray(correction.correctAnswer) ? correction.correctAnswer.join(', ') : correction.correctAnswer}</p>
         </div>
     `).join('');
+  // Scroll to the top of the page after displaying the results
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
 function restartQuiz() {
